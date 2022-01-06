cdc2657e66d0c2406f2856a23d70ca2cfd6f5f0e
Max Savitsky <maxsavitsky2015@gmail.com>
2022-01-06 18:57:06 +0300

    feat: add memory info

--- src/main/java/com/maxsavitsky/tasks/SystemStatTask.java
+++ src/main/java/com/maxsavitsky/tasks/SystemStatTask.java
@@ -5,9 +5,16 @@ import com.maxsavitsky.MessagesController;
 import com.maxsavitsky.Utils;
 import org.apache.commons.lang3.SystemUtils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.management.ManagementFactory;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SystemStatTask extends Task {
 
@@ -32,12 +39,28 @@ public class SystemStatTask extends Task {
 						"CPU usage"
 				)
 		);
+		int memoryUsage = (int) ((osBean.getTotalMemorySize() - osBean.getFreeMemorySize()) * 100 / osBean.getTotalMemorySize());
 		lines.add(
 				new Line("ram", "sys-stat",
-						((osBean.getTotalMemorySize() - osBean.getFreeMemorySize()) * 100 / osBean.getTotalMemorySize()) + "%",
+						 memoryUsage + "% "
+								 + getFormattedSize(osBean.getTotalMemorySize() - osBean.getFreeMemorySize())
+								 + "/"
+								 + getFormattedSize(osBean.getTotalMemorySize()),
 						"RAM usage"
 				)
 		);
+
+		int swapUsage = (int) ((osBean.getTotalSwapSpaceSize() - osBean.getFreeSwapSpaceSize()) * 100 / osBean.getTotalSwapSpaceSize());
+		lines.add(
+				new Line("swap-usage", "sys-stat",
+						swapUsage + "% "
+								+ getFormattedSize(osBean.getTotalSwapSpaceSize() - osBean.getFreeSwapSpaceSize())
+								+ "/"
+								+ getFormattedSize(osBean.getTotalSwapSpaceSize()),
+						"Swap usage"
+				)
+		);
+
 		if (SystemUtils.IS_OS_LINUX) {
 			String result = Utils.exec("vcgencmd measure_temp"); // temp=XX'C
 			String temp = result.substring("temp=".length());
@@ -68,8 +91,30 @@ public class SystemStatTask extends Task {
 					"ClamAV"
 			};
 			for (int i = 0; i < services.length; i++) {
-				String state = Utils.exec("systemctl is-active " + services[i]);
-				lines.add(new Line(services[i], "sys-stat", state, servicesNames[i]));
+				String state = Utils.exec("systemctl is-active " + services[i])
+						.replaceAll("\n", "");
+				String status = Utils.exec("systemctl status %s".formatted(services[i]));
+				Process process = Runtime.getRuntime().exec("grep Memory");
+				OutputStream os = process.getOutputStream();
+				os.write(status.getBytes(StandardCharsets.UTF_8));
+				os.flush();
+				os.close();
+
+				ByteArrayOutputStream bos = new ByteArrayOutputStream();
+				process.getInputStream().transferTo(bos);
+
+				String memoryStr = bos.toString();
+
+				String msg = state;
+				if(memoryStr.length() > 0){
+					String[] p = memoryStr.split(":");
+					String s = p[1].replaceAll("\n", "").trim();
+					try {
+						Double.parseDouble(s.substring(0, s.length() - 1));
+						msg += " " + s;
+					}catch (NumberFormatException ignored){}
+				}
+				lines.add(new Line(services[i], "sys-stat", msg, servicesNames[i]));
 			}
 		}
 
@@ -80,6 +125,28 @@ public class SystemStatTask extends Task {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	public static String getFormattedSize(long size){
+		double used = size;
+		String v;
+		if (used < 1024) { // bytes
+			v = "B";
+		} else if (used < 1024 * 1024) { // kilobytes
+			v = "KB";
+			used /= 1024;
+		} else if (used < 1024 * 1024 * 1024) { // megabytes
+			v = "MB";
+			used /= 1024 * 1024;
+		} else {
+			v = "GB";
+			used /= 1024 * 1024 * 1024;
+		}
+		String usedString = new BigDecimal(used).setScale(2, RoundingMode.HALF_EVEN).toString();
+		if(usedString.endsWith(".00")){
+			usedString = usedString.substring(0, usedString.length() - 3);
+		}
+		return usedString + v;
 	}
 
 }
