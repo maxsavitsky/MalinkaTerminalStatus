23a2a3fe853abe9d8b09db36578ca28d58592a75
Max Savitsky <maxsavitsky2015@gmail.com>
2022-02-21 14:03:06 +0300

    refactor: refactor MessagesSection

--- src/main/java/com/maxsavitsky/sections/MessagesSection.java
+++ src/main/java/com/maxsavitsky/sections/MessagesSection.java
@@ -8,14 +8,15 @@ import java.io.IOException;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MessagesSection extends Section {
 
 	private static final MessagesSection instance = new MessagesSection();
-	private static final int columnsOffset = Section.WIDTH / 2 + 1;
-	private static final int rowsOffset = 0;
-	private static final int columnsLimit = Section.WIDTH - columnsOffset;
-	private static final int rowsLimit = Section.HEIGHT;
+	private static final int COLUMNS_OFFSET = Section.WIDTH / 2 + 1;
+	private static final int ROWS_OFFSET = 0;
+	private static final int COLUMNS_LIMIT = Section.WIDTH - COLUMNS_OFFSET;
+	private static final int ROWS_LIMIT = Section.HEIGHT;
 	private final ArrayList<Message> messages = new ArrayList<>();
 	private int currentLineIndex = 0;
 
@@ -38,10 +39,10 @@ public class MessagesSection extends Section {
 		Message message = new Message(fullMessage);
 		messages.add(message);
 		var messageParts = message.getMessageParts();
-		int availableLines = rowsLimit - currentLineIndex + rowsOffset - 1;
+		int availableLines = ROWS_LIMIT - currentLineIndex + ROWS_OFFSET - 1;
 		if (availableLines >= message.getLinesCount()) {
 			for (int i = 0; i < messageParts.size(); i++, currentLineIndex++) {
-				terminal.setCursorPosition(new TerminalPosition(columnsOffset, currentLineIndex));
+				terminal.setCursorPosition(new TerminalPosition(COLUMNS_OFFSET, currentLineIndex));
 				terminal.putString(messageParts.get(i));
 			}
 		} else {
@@ -50,35 +51,29 @@ public class MessagesSection extends Section {
 	}
 
 	private void rewrite(Terminal terminal) throws IOException {
-		int line = rowsLimit + rowsOffset - 1;
+		int line = ROWS_LIMIT + ROWS_OFFSET - 1;
 		int messageIndex = messages.size() - 1;
-		while (line >= rowsOffset) {
+		while (line >= ROWS_OFFSET) {
 			Message m = messages.get(messageIndex);
-			for (int i = m.getLinesCount() - 1; i >= 0 && line >= rowsOffset; i--, line--) {
-				terminal.setCursorPosition(new TerminalPosition(columnsOffset, line));
+			for (int i = m.getLinesCount() - 1; i >= 0 && line >= ROWS_OFFSET; i--, line--) {
+				terminal.setCursorPosition(new TerminalPosition(COLUMNS_OFFSET, line));
 				terminal.putString(m.getMessageParts().get(i));
-				for (int j = m.getMessageParts().get(i).length(); j < columnsLimit; j++) {
+				for (int j = m.getMessageParts().get(i).length(); j < COLUMNS_LIMIT; j++) {
 					terminal.putCharacter(' ');
 				}
 			}
-			if (line >= rowsOffset)
+			if (line >= ROWS_OFFSET)
 				messageIndex--;
 		}
-	}
-
-	public void log(String message, Terminal terminal) throws IOException {
-		Line l = new Line(null, getIdentifier());
-		l.setMessage(message);
-		write(l, terminal);
 	}
 
 	private static class Message {
 		private final ArrayList<String> messageParts = new ArrayList<>();
 
 		public Message(String s) {
-			for (int i = 0; i < s.length(); i += columnsLimit) {
+			for (int i = 0; i < s.length(); i += COLUMNS_LIMIT) {
 				messageParts.add(
-						s.substring(i, Math.min(i + columnsLimit, s.length()))
+						s.substring(i, Math.min(i + COLUMNS_LIMIT, s.length()))
 				);
 			}
 		}
@@ -87,7 +82,7 @@ public class MessagesSection extends Section {
 			return messageParts.size();
 		}
 
-		public ArrayList<String> getMessageParts() {
+		public List<String> getMessageParts() {
 			return messageParts;
 		}
 	}
