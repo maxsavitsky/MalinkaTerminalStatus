62524101430ac486de4c629782c47daba6c2e3d9
Max Savitsky <maxsavitsky2015@gmail.com>
2022-02-21 13:58:30 +0300

    feat(sys-stat): add uptime and thread count metrics

--- src/main/java/com/maxsavitsky/tasks/SystemStatTask.java
+++ src/main/java/com/maxsavitsky/tasks/SystemStatTask.java
@@ -4,6 +4,7 @@ import com.maxsavitsky.Line;
 import com.maxsavitsky.MessagesController;
 import com.maxsavitsky.Utils;
 import org.apache.commons.lang3.SystemUtils;
+import oshi.SystemInfo;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -28,18 +29,19 @@ public class SystemStatTask extends Task {
 
 	@Override
 	public void execute() throws IOException {
+		String sysStatSecId = "sys-stat";
 		ArrayList<Line> lines = new ArrayList<>();
 		lines.add(
 				new Line(
 						"cpu",
-						"sys-stat",
+						sysStatSecId,
 						(int) (osBean.getCpuLoad() * 100) + "%",
 						"CPU usage"
 				)
 		);
 		int memoryUsage = (int) ((osBean.getTotalMemorySize() - osBean.getFreeMemorySize()) * 100 / osBean.getTotalMemorySize());
 		lines.add(
-				new Line("ram", "sys-stat",
+				new Line("ram", sysStatSecId,
 						 memoryUsage + "% "
 								 + getFormattedSize(osBean.getTotalMemorySize() - osBean.getFreeMemorySize())
 								 + "/"
@@ -51,7 +53,7 @@ public class SystemStatTask extends Task {
 		if(osBean.getTotalSwapSpaceSize() > 0) {
 			int swapUsage = (int) ((osBean.getTotalSwapSpaceSize() - osBean.getFreeSwapSpaceSize()) * 100 / osBean.getTotalSwapSpaceSize());
 			lines.add(
-					new Line("swap-usage", "sys-stat",
+					new Line("swap-usage", sysStatSecId,
 							swapUsage + "% "
 									+ getFormattedSize(osBean.getTotalSwapSpaceSize() - osBean.getFreeSwapSpaceSize())
 									+ "/"
@@ -61,63 +63,34 @@ public class SystemStatTask extends Task {
 			);
 		}
 
+		SystemInfo systemInfo = new SystemInfo();
+		long uptime = systemInfo.getOperatingSystem().getSystemUptime();
+		String uptimeString = (uptime % 60) + "s";
+		uptime /= 60;
+		if(uptime > 0){
+			uptimeString = (uptime % 60) + "m " + uptimeString;
+			uptime /= 60;
+		}
+		if(uptime > 0){
+			uptimeString = uptime + "h " + uptimeString;
+			uptime /= 24;
+		}
+		if(uptime > 0){
+			uptimeString = uptime + "d " + uptimeString;
+		}
+		lines.add(new Line("uptime", sysStatSecId, uptimeString, "Uptime"));
+
+		lines.add(new Line("thread-count", sysStatSecId, "" + systemInfo.getOperatingSystem().getThreadCount(), "Thread count"));
+
 		if (SystemUtils.IS_OS_LINUX) {
 			String result = Utils.exec("vcgencmd measure_temp"); // temp=XX'C
 			String temp = result.substring("temp=".length());
 			lines.add(
-					new Line("Temp", "sys-stat", temp, "Temp")
+					new Line("Temp", sysStatSecId, temp, "Temp")
 			);
 
 			if(enableServicesStats) {
-				String[] services = new String[]{
-						"mstsite",
-						"mstauth",
-						"mstdrive",
-						"gitea",
-						"nginx",
-						"mysql",
-						"postfix",
-						"dovecot",
-						"clamav-daemon"
-				};
-				String[] servicesNames = new String[]{
-						"Site",
-						"Auth",
-						"Drive",
-						"Gitea",
-						"nginx",
-						"mysql",
-						"Postfix",
-						"Dovecot",
-						"ClamAV"
-				};
-				for (int i = 0; i < services.length; i++) {
-					String state = Utils.exec("systemctl is-active " + services[i])
-							.replaceAll("\n", "");
-					String status = Utils.exec("systemctl status %s".formatted(services[i]));
-					Process process = Runtime.getRuntime().exec("grep Memory");
-					OutputStream os = process.getOutputStream();
-					os.write(status.getBytes(StandardCharsets.UTF_8));
-					os.flush();
-					os.close();
-
-					ByteArrayOutputStream bos = new ByteArrayOutputStream();
-					process.getInputStream().transferTo(bos);
-
-					String memoryStr = bos.toString();
-
-					String msg = state;
-					if (memoryStr.length() > 0) {
-						String[] p = memoryStr.split(":");
-						String s = p[1].replaceAll("\n", "").trim();
-						try {
-							Double.parseDouble(s.substring(0, s.length() - 1));
-							msg += " " + s;
-						} catch (NumberFormatException ignored) {
-						}
-					}
-					lines.add(new Line(services[i], "sys-stat", msg, servicesNames[i]));
-				}
+				lines.addAll(getServicesStats());
 			}
 		}
 
@@ -128,6 +101,61 @@ public class SystemStatTask extends Task {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private static ArrayList<Line> getServicesStats() throws IOException {
+		ArrayList<Line> lines = new ArrayList<>();
+		String[] services = new String[]{
+				"mstsite",
+				"mstauth",
+				"mstdrive",
+				"gitea",
+				"nginx",
+				"mysql",
+				"postfix",
+				"dovecot",
+				"clamav-daemon"
+		};
+		String[] servicesNames = new String[]{
+				"Site",
+				"Auth",
+				"Drive",
+				"Gitea",
+				"nginx",
+				"mysql",
+				"Postfix",
+				"Dovecot",
+				"ClamAV"
+		};
+		for (int i = 0; i < services.length; i++) {
+			String state = Utils.exec("systemctl is-active " + services[i])
+					.replace("\n", "");
+			String status = Utils.exec("systemctl status %s".formatted(services[i]));
+			Process process = Runtime.getRuntime().exec("grep Memory");
+			OutputStream os = process.getOutputStream();
+			os.write(status.getBytes(StandardCharsets.UTF_8));
+			os.flush();
+			os.close();
+
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			process.getInputStream().transferTo(bos);
+
+			String memoryStr = bos.toString();
+
+			String msg = state;
+			if (memoryStr.length() > 0) {
+				String[] p = memoryStr.split(":");
+				String s = p[1].replace("\n", "").trim();
+				try {
+					Double.parseDouble(s.substring(0, s.length() - 1));
+					msg += " " + s;
+				} catch (NumberFormatException ignored) {
+					// ignore
+				}
+			}
+			lines.add(new Line(services[i], "sys-stat", msg, servicesNames[i]));
+		}
+		return lines;
 	}
 
 	public static String getFormattedSize(long size){
@@ -145,7 +173,7 @@ public class SystemStatTask extends Task {
 			v = "GB";
 			used /= 1024 * 1024 * 1024;
 		}
-		String usedString = new BigDecimal(used).setScale(2, RoundingMode.HALF_EVEN).toString();
+		String usedString = BigDecimal.valueOf(used).setScale(2, RoundingMode.HALF_EVEN).toString();
 		if(usedString.endsWith(".00")){
 			usedString = usedString.substring(0, usedString.length() - 3);
 		}
