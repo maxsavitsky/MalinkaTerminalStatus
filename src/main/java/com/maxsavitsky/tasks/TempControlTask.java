42246c3ca108b34722e23da90e71c453514c9d7e
Max Savitsky <maxsavitsky2015@gmail.com>
2022-03-01 22:41:31 +0300

    feat: add shutdown command for different os families

--- src/main/java/com/maxsavitsky/tasks/TempControlTask.java
+++ src/main/java/com/maxsavitsky/tasks/TempControlTask.java
@@ -5,9 +5,9 @@ import com.maxsavitsky.MailSender;
 import com.maxsavitsky.Main;
 import com.maxsavitsky.MessagesController;
 import com.maxsavitsky.Utils;
-import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import oshi.SystemInfo;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
@@ -16,57 +16,66 @@ public class TempControlTask extends Task {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	public static final long TIMER_PERIOD = 1000 * 60L;
-
 	private static final int SHUTDOWN_TEMP = 80;
 
 	private static final int NOTIFICATION_TEMP = 70;
 
 	private long lastTemperaturePrintTime = 0;
 
+	private void printMessage(String message) throws IOException {
+		MessagesController.handle(new Line(
+				null,
+				"msg",
+				message
+		));
+	}
+
+	private void sendMail(String title, String message) {
+		try {
+			if (MailSender.getInstance() != null)
+				MailSender.getInstance().sendToAdmin(title, message);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	public void execute() throws IOException {
 		double temp = Main.getSystemInfo().getHardware().getSensors().getCpuTemperature();
-		if(temp == 0 || Double.isNaN(temp)){
+		if (temp == 0 || Double.isNaN(temp)) {
 			logger.error("Failed to get temperature");
-			MessagesController.handle(new Line(
-					null,
-					"msg",
-					"Failed to get temperature"
-			));
+			printMessage("Failed to get temperature");
 			return;
 		}
 		long time = System.currentTimeMillis();
-		if(time - lastTemperaturePrintTime >= 5L * 60 * 1000) {
+		if (time - lastTemperaturePrintTime >= 5L * 60 * 1000) {
 			logger.info("Current temperature is {}", temp);
 			lastTemperaturePrintTime = time;
 		}
-		if(temp >= NOTIFICATION_TEMP){
-			MessagesController.handle(new Line(
-					null,
-					"msg",
-					"Temp warning: " + temp
-			));
-			try {
-				if(MailSender.getInstance() != null)
-					MailSender.getInstance().sendToAdmin("TEMPERATURE WARNING", "Current temperature is " + temp + "°C");
-			} catch (MessagingException e) {
-				e.printStackTrace();
-			}
+		if (temp >= NOTIFICATION_TEMP) {
+			printMessage("Temp warning: " + temp);
+			sendMail("TEMPERATURE WARNING", "Current temperature is " + temp + "°C");
 		}
 		if (temp >= SHUTDOWN_TEMP) {
 			logger.warn("TEMPERATURE IS HIGH THAN {}. SHUTDOWN", SHUTDOWN_TEMP);
-			try {
-				if(MailSender.getInstance() != null)
-					MailSender.getInstance().sendToAdmin("OVERTEMPERATURE", "Temperature is " + temp + "°C. Limit is " + SHUTDOWN_TEMP + "°C. SHUTDOWN");
-			} catch (MessagingException e) {
-				e.printStackTrace();
-			}
-			if (SystemUtils.IS_OS_LINUX) {
-				Utils.exec("shutdown -h now");
+			sendMail("OVERHEATING", "Temperature is " + temp + "°C. Limit is " + SHUTDOWN_TEMP + "°C. SHUTDOWN");
+			String shutdownCommand = getShutdownCommand();
+			if (shutdownCommand == null) {
+				printMessage("Cannot get shutdown command for current OS: " + SystemInfo.getCurrentPlatform());
+			} else {
+				Utils.exec(shutdownCommand);
 				System.exit(0);
 			}
 		}
+	}
+
+	private String getShutdownCommand() {
+		return switch (SystemInfo.getCurrentPlatform()) {
+			case LINUX, MACOS -> "shutdown -h now";
+			case FREEBSD, KFREEBSD, NETBSD, OPENBSD -> "halt";
+			case WINDOWS, WINDOWSCE -> "shutdown /s /f /t 5";
+			default -> null;
+		};
 	}
 
 
